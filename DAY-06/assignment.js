df9505b2ac89877967d90d8e612f563397878fb0
Habib <habib@bintofajjal.com>
2025-08-24 17:28:54 +0600

    [Day-06] Task 7 is completed but Task 6 is not

--- DAY-06/assignment.js
+++ DAY-06/assignment.js
@@ -1,8 +1,7 @@
 console.log('Day 6 Assignment Script is loaded')
 /*
 
-7. Use an IIFE to Print “Hello, JavaScript!”
-Write an IIFE that prints "Hello, JavaScript!" to the console. Here the Second word must be supplied using paramneter and argument.
+
 
 8. Create a Simple Callback Function
 Write a function greet(name, callback), where callback prints a message using the name parameter.
@@ -101,3 +100,15 @@ console.log("Total Vowels:", total)
 6. Write a Function to Capitalize the First Letter of Each Word in a Sentence
 Write a function capitalizeWords(sentence) that takes a sentence and capitalizes the first letter of each word. You can use the toUpperCase() method of string to convert the lowercase to uppercase.
 */
+console.log("=====> TASK 6 ❌ is incompleted due to lack of knowledge <=====")
+
+/*
+7. Use an IIFE to Print “Hello, JavaScript!”
+Write an IIFE that prints "Hello, JavaScript!" to the console. Here the Second word must be supplied using paramneter and argument.
+*/
+
+let xyz = function(a){
+    console.log("Hello " + a)
+}
+xyz("JavaScript")
+
